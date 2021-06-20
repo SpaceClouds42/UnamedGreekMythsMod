b5475836c2591141defae46260c8be357936e56e
P03W <P03W@users.noreply.github.com>
2021-06-20 16:59:14 -0400

    Cleaner code, ignore creative/spectator

--- src/main/kotlin/us/spaceclouds42/greek/myths/effect/PetrificationEffect.kt
+++ src/main/kotlin/us/spaceclouds42/greek/myths/effect/PetrificationEffect.kt
@@ -4,8 +4,6 @@ import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectType
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ItemStack
-import us.spaceclouds42.greek.myths.mixin.CombinedInventoryAccessor
 import us.spaceclouds42.greek.myths.util.combined
 import kotlin.random.Random
 
@@ -16,9 +14,9 @@ class PetrificationEffect : StatusEffect(StatusEffectType.HARMFUL, 0x4f3d25) {
         if (entity.hurtTime <= 0 && !entity.isInvulnerable) {
             entity.damage(petrificationDamage, 1.0f)
 
-            repeat(4) {
-                if (entity is PlayerEntity && !entity.world.isClient) {
-                    val combined = entity.inventory.combined
+            if (entity is PlayerEntity && !entity.world.isClient && !entity.isCreative && !entity.isSpectator) {
+                val combined = entity.inventory.combined
+                repeat(4) {
                     entity.dropItem(entity.inventory.removeStack(Random.nextInt(0, combined.size)), true, false)
                 }
             }
